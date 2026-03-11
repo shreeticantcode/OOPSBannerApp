01cff7932f6ffd182b9bdaee3b1ac343124b8be8
shreeticantcode <shreetireni27@gmail.com>
2026-03-11 13:28:00 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+public class OOPSBannerApp {
+
+    /**
+     * Main method – application entry point
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
